44a42964301250921372ec742eff977803e4e587
Brandon Kong <bkong28@gmail.com>
2014-07-08 01:01:50 +1000

    Implemented profile pages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show,
+                                   :edit,
+                                   :update,
+                                   :destroy]
+
   def new
   	@user = User.new
   end
@@ -6,21 +11,42 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
 		 
-		  if @user.save
-		    flash[:notice] = "You have signed up successfully."
-		    redirect_to questions_path
-		  else
-		    render :new
+	  if @user.save
+	    flash[:notice] = "You have signed up successfully."
+	    redirect_to questions_path
+	  else
+	    render :new
 		end
   end
 
   def show
   end
 
+  def edit
+  end
+
+  def update
+		if @user.update(user_params)
+		  flash[:notice] = "Profile has been updated."
+		  redirect_to @user
+		else
+	    flash[:alert] = "User has not been updated."
+	    render "edit"
+	  end
+  end
+  
   private
 	  def user_params
 			params.require(:user).permit(:name,
 																	:password,
 																	:password_confirmation)
-		end	
+		end
+
+		def set_user
+	    @user = User.find(params[:id])
+	  rescue ActiveRecord::RecordNotFound
+	    flash[:alert] = "The user you were looking" + " for could not be found."
+	    redirect_to users_path
+	  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 LingoAmigo::Application.routes.draw do
-  get "users/new"
-  get "users/create"
-  get "users/show"
+
   root "questions#index"
+  
   resources :questions do
   	resources :answers
   end
+  
   resources :users
 end
